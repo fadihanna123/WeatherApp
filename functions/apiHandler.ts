f8c9523a3a910b526349122022587cdf5e70b937
FadiHanna <fadi.hanna@rektron.se>
2022-09-25 05:26:10 +0200

    Fix all bugs

--- functions/apiHandler.ts
+++ functions/apiHandler.ts
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import * as Location from 'expo-location';
 
 import { request } from '../api';
--- functions/getCurrentLocation.ts
+++ functions/getCurrentLocation.ts
@@ -1,6 +1,5 @@
 import * as Location from 'expo-location';
 import { Dispatch } from 'react';
-import { Alert } from 'react-native';
 
 import {
   setCityInput,
