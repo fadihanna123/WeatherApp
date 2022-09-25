d940ba829ed1ee5d16234bf78558b38a0cd1825d
FadiHanna <fadi.hanna@rektron.se>
2022-09-25 05:24:04 +0200

    Fix all bugs

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import axios from 'axios';
-import React, { Dispatch, useEffect } from 'react';
-import { Provider, useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { Provider } from 'react-redux';
 
 import Layout from './app/Layout';
-import { getCurrentLocation } from './functions';
-import { ViewCurrentReducerTypes } from './models';
 import { store } from './redux/store';
 
 const globalHeader: string = 'application/json';
-const baseURL = 'http://api.weatherstack.com/';
+const baseURL: string = 'http://api.weatherstack.com/';
 
 axios.defaults.baseURL = baseURL;
 axios.defaults.headers.common['Content-Type'] = globalHeader;
